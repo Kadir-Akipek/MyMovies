3425e6f439a6ec1a5c26855f0733128016dda7ea
Kadir-Akipek <110154102+Kadir-Akipek@users.noreply.github.com>
2023-11-10 20:51:41 +0300

    17. video bitti

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from "react";
-import SearchBar from './SearchBar'
-import MovieList from './MovieList'
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 //Listeleyeceğimiz film sayısı belli olmadığı için state kullanacağız, state içinde class component kullanacağız
 //state bir obje olduğu için, state içerisindeki array property olacaktır
+//Parent Component(App.js)' dan child component(MovieList)'a geçmemizin en kolay yolu MovieList'e prop oluşturmaktır
 
 class  App extends React.Component {
 
@@ -16,7 +17,6 @@ class  App extends React.Component {
               "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
               "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
             },
-    
             {
               "id": 2,
               "name": "Interstellar",
@@ -30,20 +30,47 @@ class  App extends React.Component {
                 "rating": 7.9,
                 "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
                 "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-              }
-                ]
-     }  
+            }
+        ],
+
+        searchQuery: ""
+    }  
+
+    deleteMovie = (movie) => {
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        );
+
+        this.setState(state => ({
+            movies: newMovieList
+        }))
+    }
+
+    searchMovie = (event) => {
+        //console.log(event.target.value)
+        this.setState({searchQuery: event.target.value})
+    }
 
     render() {
+
+        let filteredMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+            }
+        )
 
         return(
             <div className="container">
                 <div className="row">
-                    <dic className="col-lg-12">
-                        <SearchBar />
-                    </dic>
+                    <div className="col-lg-12">
+                        <SearchBar 
+                            searchMovieProp = {this.searchMovie}
+                        />
+                    </div>
                 </div>
-                <MovieList />
+                <MovieList 
+                movies={filteredMovies} 
+                deleteMovieProp = {this.deleteMovie}    />
             </div>
         );
     }
--- src/components/movieList.js
+++ src/components/movieList.js
@@ -1,30 +1,41 @@
 import React from "react";
 
 //Bir boostrap sütunu 12 class'tan oluşur, biz bunu 3'e böleceğiz o yüzden col-lg-4 yazıyoruz
+//Yazdığımız fonksiyonun, uygulama başlatıldığı an çalıştırmak istersek fonsiyonun sonuna () eklemeliyiz
 
-class MovieList extends React.Component {
-    render() {
+const MovieList = (props) =>  {
+
+    /*function handleClick(event) {
+        //console.log("Button clicked");
+        console.log(event.pageX)
+    }*/
 
         return (
             <div className="row">
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src="https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg" className="card-img-top" alt="Sample Movie"/>
-                        <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="card-text">Sample Movie Description</p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2><span className="bedge bedge-info">9.0</span></h2>
+
+                {props.movies.map((movie) => (
+                    <div className="col-lg-4" key={movie.id}>
+                        <div className="card mb-4 shadow-sm">
+                            <img src={movie.imageURL} className="card-img-top" alt="Sample Movie" />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.name}</h5>
+                                <p className="card-text">{movie.overview}</p>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <button type="button" onClick={(event) => props.deleteMovieProp(movie)} className="btn btn-md btn-outline-danger">
+                                        Delete
+                                    </button>
+                                    <h2>
+                                        <span className="badge badge-info" style={{ backgroundColor: 'aqua' }}>
+                                            {movie.rating}
+                                        </span>
+                                    </h2>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div>
         );
-    }
 }
 
 export default MovieList;
-
-
